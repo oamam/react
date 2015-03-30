04f51dfb780992e4f822e7bf8ed50239f7c4657f
masashi0127 <chapa0106@gmail.com>
2015-03-30 01:52:53 +0100

    modified

--- admin.js
+++ admin.js
@@ -1,43 +1,60 @@
-var randobet = function(n, b) {
-    b = b || '';
-    var a = 'abcdefghijklmnopqrstuvwxyz'
-        + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        + '0123456789'
-        + b;
-    a = a.split('');
-    var s = '';
-    for (var i = 0; i < n; i++) {
-        s += a[Math.floor(Math.random() * a.length)];
-    }
-    return s;
-};
-
-var orders = [
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'},
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'},
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'},
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'},
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'},
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'},
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'},
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'},
-    {'trading_id': randobet(10),'first_name': 'masashi','last_name': 'amao','email': 'example@google.com','phone': '123456789','number': '10','zip': '123456789','address_line_1': 'address_line_1','address_line_2': 'address_line_2','city': 'city','spr': 'spr','country': 'country','created': '2000-01-01 00:00:00','modified': '2000-01-01 00:00:00'}
-];
 
 var SearchForm = React.createClass({
+    getInitialState: function() {
+        return {
+            searchStatus: '',
+            searchText: ''
+        }
+    },
     handleSearch: function() {
-        this.props.onSearch(this.refs.searchText.getDOMNode().value);
+        var searchText = this.refs.searchText.getDOMNode().value;
+        var searchStatus = this.refs.searchStatus.getDOMNode().value;
+
+        this.setState({
+            searchText: searchText,
+            searchStatus: searchStatus
+        });
+        
+        this.props.onSearch(searchText, searchStatus);
     },
     render: function() {
+        var statusSelector = '';
+
+        if (admin.type == 1) {
+            statusSelector = React.createElement(
+                'select',
+                {className: 'search-status', ref: 'searchStatus', onChange: this.handleSearch, value: this.state.searchStatus},
+                React.createElement('option', {value: ''}, 'すべて'),
+                React.createElement('option', {value: 0}, '未対応'),
+                React.createElement('option', {value: 1}, '出荷可能'),
+                React.createElement('option', {value: 9}, 'キャンセル')
+            );
+
+        } else if (admin.type == 2) {
+            statusSelector = React.createElement(
+                'select',
+                {className: 'search-status', ref: 'searchStatus', onChange: this.handleSearch, value: this.state.searchStatus},
+                React.createElement('option', {value: ''}, 'すべて'),
+                React.createElement('option', {value: 1}, '出荷可能'),
+                React.createElement('option', {value: 2}, '出荷済')
+            );
+
+        } else {
+            statusSelector = React.createElement(
+                'select',
+                {className: 'search-status', ref: 'searchStatus', onChange: this.handleSearch, value: this.state.searchStatus},
+                React.createElement('option', {value: ''}, 'すべて'),
+                React.createElement('option', {value: 0}, '未対応'),
+                React.createElement('option', {value: 1}, '出荷可能'),
+                React.createElement('option', {value: 2}, '出荷済'),
+                React.createElement('option', {value: 9}, 'キャンセル')
+            );
+        }
+
         return (
             <form className="search-form">
-                <input type="text" className="search-text" placeholder="キーワードを入力してください" ref="searchText" onChange={this.handleSearch} value={this.props.searchText} />
-                <select className="search-status" ref="searchStatus" onChange={this.handleSearch} value={this.state.searchStatus}>
-                    <option value="">すべて</option>
-                    <option value="0">未対応</option>
-                    <option value="1">発注手続済</option>
-                    <option value="9">キャンセル</option>
-                </select>
+                <input type="text" className="search-text" placeholder="キーワードを入力してください" ref="searchText" onChange={this.handleSearch} value={this.state.searchText} />
+                {statusSelector}
             </form>
         );
     }
@@ -83,7 +100,13 @@ var Order = React.createClass({
             ':' + ('0' + nowDate.getSeconds()).slice(-2);
     },
     __getselectedStatus: function(status) {
-        var options = [{text: '未対応', value: 0, selectedStatus: 'non-support'}, {text: '発注手続済', value: 1, selectedStatus: 'complete'}, {text: 'キャンセル', value: 9, selectedStatus: 'cancel'}];
+        var options = [
+            {text: '未対応', value: 0, selectedStatus: 'non-support'},
+            {text: '出荷可能', value: 1, selectedStatus: 'shipment-enable'},
+            {text: '出荷済', value: 2, selectedStatus: 'shipment-complete'},
+            {text: 'キャンセル', value: 9, selectedStatus: 'cancel'}
+        ];
+        
         var selectedStatus = '';
 
         for (i = 0, l = options.length; i < l; i++) {
@@ -96,27 +119,54 @@ var Order = React.createClass({
         return selectedStatus;
     },
     render: function() {
+        var statusSelector = '';
+
+        if (admin.type == 1) {
+            statusSelector = React.createElement(
+                'select',
+                {className: 'order__status', ref: 'orderStatus', onChange: this.handlerStatusChange, value: this.state.status},
+                React.createElement('option', {value: 0}, '未対応'),
+                React.createElement('option', {value: 1}, '出荷可能'),
+                React.createElement('option', {value: 9}, 'キャンセル')
+            );
+
+        } else if (admin.type == 2) {
+            statusSelector = React.createElement(
+                'select',
+                {className: 'order__status', ref: 'orderStatus', onChange: this.handlerStatusChange, value: this.state.status},
+                React.createElement('option', {value: 1}, '出荷可能'),
+                React.createElement('option', {value: 2}, '出荷済')
+            );
+
+        } else {
+            statusSelector = React.createElement(
+                'select',
+                {className: 'order__status', ref: 'orderStatus', onChange: this.handlerStatusChange, value: this.state.status},
+                React.createElement('option', {value: 0}, '未対応'),
+                React.createElement('option', {value: 1}, '出荷可能'),
+                React.createElement('option', {value: 2}, '出荷済'),
+                React.createElement('option', {value: 9}, 'キャンセル')
+            );
+        }
+
         return (
-            <li className={this.state.selectedStatus} ref="order">
+            <li className={this.state.className} ref="order">
                 <div className="order-column">
                     <p className="order__trading-id">{this.props.tradingId}</p>
-                    <select className="order__status" ref="orderStatus" onChange={this.handlerStatusChange} value={this.state.status}>
-                        <option value="0">未対応</option>
-                        <option value="1">発注手続済</option>
-                        <option value="9">キャンセル</option>
-                    </select>
+                    {statusSelector}
                     <p className="order__message" ref="orderMessage">{this.state.message}</p>
                 </div>
                 <div className="order-column">
                     <p className="order__text">{this.props.userName}</p>
-                    <p className="order__text">{this.props.email}</p>
-                    <p className="order__text">{this.props.phone}</p>
-                    <p className="order__text">{this.props.number}</p>
-                </div>
-                <div className="order-column">
                     <p className="order__text">{this.props.zip}</p>
                     <p className="order__text">{this.props.addressLine}</p>
                     <p className="order__text">{this.props.mainAddress}</p>
+                </div>
+                <div className="order-column">
+                    <p className="order__text">{this.props.email}</p>
+                    <p className="order__text">{this.props.phone}</p>
+                    <p className="order__text">{this.props.quantity}</p>
+                    <p className="order__text">{this.props.totalPrice}</p>
                 </div>
                 <div className="order-column">
                     <p className="order__text">{this.props.created}</p>
@@ -139,7 +189,8 @@ var OrderList = React.createClass({
             order.last_name.toLowerCase().indexOf(this.props.searchText.toLowerCase()) >= 0 ||
             order.email.indexOf(this.props.searchText) >= 0 ||
             order.phone.indexOf(this.props.searchText) >= 0 ||
-            order.number.indexOf(this.props.searchText) >= 0 ||
+            order.quantity.indexOf(this.props.searchText) >= 0 ||
+            order.total_price.indexOf(this.props.searchText) >= 0 ||
             order.zip.indexOf(this.props.searchText) >= 0 ||
             order.address_line_1.indexOf(this.props.searchText) >= 0 ||
             order.address_line_1.toLowerCase().indexOf(this.props.searchText.toLowerCase()) >= 0 ||
@@ -163,23 +214,44 @@ var OrderList = React.createClass({
         }
     },
 
-    componentDidMount: function() {
-        this.setState({
-            orders: orders
+    loadOrdersFromServer: function() {
+        $.ajax({
+            url: 'admin.php',
+            dataType: 'json',
+            type: 'GET',
+            success: function(orders) {
+                this.setState({
+                    orders: orders
+                });
+            }.bind(this),
+            error: function() {
+                console.log('ng');
+            }.bind(this)
         });
     },
 
+    componentDidMount: function() {
+       this.loadOrdersFromServer();
+    },
+
     handlerStatusChange: function(tradingId, status, modified) {
-        var i, l;
-        for (i = 0, l = orders.length; i < l; i++) {
-            if (orders[i].trading_id == tradingId) {
-                console.log(status);
-                orders[i].status = status;
-                orders[i].modified = modified;
-                break;
-            }
-        }
-        return true;
+        $.ajax({
+            url: 'admin.php',
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                status: status,
+                trading_id: tradingId,
+                modified: modified
+            },
+            success: function(data) {
+                this.loadOrdersFromServer();
+            }.bind(this),
+            error: function() {
+                this.setState({msg: '更新できませんでした。'});
+                setTimeout(this.__hideUpdateMessage, 2000);
+            }.bind(this)
+        });
     },
 
     render: function() {
@@ -197,10 +269,11 @@ var OrderList = React.createClass({
                         userName: order.first_name + ' ' + order.last_name,
                         email: order.email,
                         phone: order.phone,
-                        number: order.number,
+                        quantity: order.quantity,
                         zip: order.zip,
                         addressLine: order.address_line_1 + ' ' + order.address_line_2,
                         mainAddress: order.city + ' ' + order.spr + ' ' + order.country,
+                        totalPrice: order.total_price,
                         status: order.status,
                         created: order.created,
                         modified: order.modified,
@@ -236,6 +309,7 @@ var Section = React.createClass({
         return (
             <section className="section">
                 <SearchForm searchText={this.state.searchText} searchStatus={this.state.searchStatus} onSearch={this.handleSearch} />
+                <div className="section-logout"><a href="./logout.php" className="section-logout__button">ログアウト</a></div>
                 <OrderList searchText={this.state.searchText} searchStatus={this.state.searchStatus} />
             </section>
         );
